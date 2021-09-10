b8304758a82ee81ede8190212fef2aa81d5c85f9
Loka017 <jessinthespace@gmail.com>
2021-09-11 00:09:12 +0900

    버튼 누르면 로그인 로직 구현

--- app/src/main/java/com/ateam/uikotlinpractice_login_loka/MainActivity.kt
+++ app/src/main/java/com/ateam/uikotlinpractice_login_loka/MainActivity.kt
@@ -2,10 +2,27 @@ package com.ateam.uikotlinpractice_login_loka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//       로그인 버튼 클릭 했을 때
+        loginBtn.setOnClickListener { 
+            
+//            id와 비번이 일치하면 토스트로 "관리자 입니다" 메세지 보여준다 
+            val getId = idEdt.text.toString()
+            val getPass = passEdt.text.toString()
+            
+            if(getId == "admin@test.com" && getPass == "qwer"){
+                Toast.makeText(this, "관리자 입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "넌 아니야", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
